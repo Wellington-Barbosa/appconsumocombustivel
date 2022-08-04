0180e3c7c61bceeade3168942985ee353d4e4655
Wellington Barbosa <wellington_ueg@hotmail.com>
2022-08-04 09:02:29 -0300

    Projeto criado para análise de consumo de combustíveis

---
+++ app/src/main/java/com/wellington_barbosa/app_consumo_combustivel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wellington_barbosa.app_consumo_combustivel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
