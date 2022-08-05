0bdf68129b1d07b0d81f12ded54154fe671958e3
Wellington Barbosa <wellington_ueg@hotmail.com>
2022-08-05 01:03:50 -0300

    Gradle: realizado a importação da extensão kotlin para facilitar o desenvolvimento.
    Manifest: determinado que o app somente irá operar em modo retrato.
    
    As demais alterações foram códigos relacionados ao projeto inicial.

--- app/src/main/java/com/wellington_barbosa/app_consumo_combustivel/MainActivity.kt
+++ app/src/main/java/com/wellington_barbosa/app_consumo_combustivel/MainActivity.kt
@@ -2,10 +2,18 @@ package com.wellington_barbosa.app_consumo_combustivel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btnCalcular(view: View) {
+        val precoEtanol = txtEtanol.text.toString()
+        val precoGasolina = txtGasolina.text.toString()
     }
 }
